dad2c88bcf1bb9219f6b0c8ef672f800edb77356
Joana <joanamartasouza@gmail.com>
2025-10-19 00:35:17 -0300

    19/10/2025

--- app/src/main/java/com/example/imparktcc/ui/viewmodel/CadastroUiState.kt
+++ app/src/main/java/com/example/imparktcc/ui/viewmodel/CadastroUiState.kt
@@ -1,5 +1,8 @@
 package com.example.imparktcc.ui.viewmodel
 
+import android.R
+import android.provider.ContactsContract.DisplayNameSources.EMAIL
+
 
 data class CadastroUiState(
     // Campos do formulário
@@ -51,5 +54,66 @@ data class CadastroUiState(
     //Calcula a forca da senha (0-4)
     val forcaSenha: Int
         get() = calculatePasswordStrength(senha)
+    val tempoProcessoCadastro: Long
+        get() = if (timestampFimCadastro > timetampInicioCadastro){
+            (timestampFimCadastro - timetampInicioCadastro) / 1000
+        }else 0L
+    val exibirAvisoTentativas: Boolean
+        get() = tentativasCadastro >= 3 && !cadastroSucesso
+    val progressoCadastro: Int
+        get() = when{
+            cadastroSucesso -> 100
+            isLoading -> 50
+            formularioValido -> 25
+            else -> 0
+        }
+    fun copyComValidacao(
+        nome: String = this.nome,
+        email: String = this.email,
+        senha: String = this.senha,
+       confirmarSenha: String = this.confirmarSenha
+    ): CadastroUiState {
+        return this.copy(
+            nome = nome,
+            email = email,
+            senha = senha,
+            confirmarSenha = confirmarSenha,
+            // Revalida todos os campos
+            camposPreenchidos = nome.isNotBlank() && email.isNotBlank() &&
+                    senha.isNotBlank() && confirmarSenha.isNotBlank(),
+            emailValido = isValidEmail(email),
+            senhaValida = isValidPassword(senha),
+            senhasCoincidem = senha == confirmarSenha && senha.isNotBlank()
+        )
+    }
+    fun copyComLoading(): CadastroUiState{
+        return this.copy(
+            isLoading = true,
+            mensagemErro = "",
+            timestampInicioCadastro = System.currentTimeMillis()
+        )
+    }
+    fun copyComSucesso(): CadastroUiState{
+        return this.copy(
+            isLoading = false,
+            cadastroSucesso = true,
+            mensagemErro = "",
+            timestampFimCadastro = System.currentTimeMillis()
+
+        )
+    }
+    fun copyComFoco(campo: CampoFocado): CadastroUiState{
+           return this.copy(campoComFoco = campo)
+    }
+    fun copyComEstadoConexao(conectado: Boolean): CadastroUiState{
+        return this.copy(conexaoInternet = conectado)
+
+    }
+    fun getMensagemErro(campo: CampoFocado? = null):String?{
+        return while (campo ?: campoComFoco){
+            CampoFocado.NOME -> if(nome.isBlank()) "Nome é obrigatorio" else null
+            CampoFocado>EMAIL -> if
+        }
+    }
 
 }
